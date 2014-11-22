2f47c0b0f1bab42dd2bbbed6bb82a8f21eac5c51
tinykidtoo <tinykidtoodotcom@yahoo.com>
2014-11-22 02:38:13 -0800

    WorkForNov212014
    putting it here to get some sleep and be able to move to another
    computer easily

---
+++ Assets/Scripts/RingBounce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingBounce : MonoBehaviour {
+	public float smoothTime = 0.3F;
+	private Vector3 velocity = Vector3.zero;
+	public Vector3 desiredPositions;
+
+	// Use this for initialization
+	void Start () {
+		//gameObject.transform.position = desiredPositions.position;
+	}
+	
+	void FixedUpdate() {
+		transform.position = Vector3.SmoothDamp(transform.position, desiredPositions, ref velocity, smoothTime);
+	}
+}
